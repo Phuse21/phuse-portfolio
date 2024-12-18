5f7594cffff723df4a1fbe4d1ed4170122960842
Phuse21 <phusedallas@gmail.com>
2024-12-17 22:18:06 -0500

    resume page- skills

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -7,6 +7,12 @@ import { SiTailwindcss, SiLaravel, SiNextdotjs } from "react-icons/si";
 import { motion } from "framer-motion";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 //about data
 const about = {
@@ -130,7 +136,7 @@ const skills = {
     },
 
     {
-      name: "React",
+      name: "React.js",
       icon: <FaReact />,
     },
     {
@@ -238,7 +244,36 @@ const Resume = () => {
 
             {/* skills */}
             <TabsContent value="skills" className="w-full">
-              Skills
+              <div className="flex flex-col gap-[30px]">
+                <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                  <h3 className="text-4xl font-bold">{skills.title}</h3>
+                  <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                    {skills.description}
+                  </p>
+
+                  <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 xl:gap-[30px] ">
+                    {skills.skillsList.map((skill, index) => {
+                      return (
+                        <li key={index}>
+                          <TooltipProvider delayDuration={100}>
+                            <Tooltip>
+                              <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group">
+                                <div className="text-6xl group-hover:text-accent transition-all duration-300">
+                                  {skill.icon}
+                                </div>
+                              </TooltipTrigger>
+
+                              <TooltipContent>
+                                <p className="capitalize">{skill.name}</p>
+                              </TooltipContent>
+                            </Tooltip>
+                          </TooltipProvider>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              </div>
             </TabsContent>
             {/* About Me */}
             <TabsContent value="about" className="w-full">
