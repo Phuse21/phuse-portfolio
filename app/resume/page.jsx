b00d3e7714fe316da2f88ef1763bca1972fcf1fa
Phuse21 <phusedallas@gmail.com>
2024-12-17 22:31:25 -0500

    resume page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -276,8 +276,29 @@ const Resume = () => {
               </div>
             </TabsContent>
             {/* About Me */}
-            <TabsContent value="about" className="w-full">
-              About Me
+            <TabsContent
+              value="about"
+              className="w-full text-center xl:text-left"
+            >
+              <div className="flex flex-col gap-[30px]">
+                <h3 className="text-4xl font-bold">{about.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {about.description}
+                </p>
+                <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0">
+                  {about.info.map((info, index) => {
+                    return (
+                      <li
+                        key={index}
+                        className="flex items-center justify-center xl:justify-start gap-4"
+                      >
+                        <span className="text-white/60">{info.fieldName}</span>
+                        <span className="text-xl">{info.value}</span>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
             </TabsContent>
           </div>
         </Tabs>
