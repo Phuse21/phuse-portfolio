b92c9537049e053d7205dd1318a533a3c31f74a6
Phuse21 <phusedallas@gmail.com>
2024-12-17 21:51:28 -0500

    resume page- exxperience

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -6,6 +6,7 @@ import { SiTailwindcss, SiLaravel, SiNextdotjs } from "react-icons/si";
 
 import { motion } from "framer-motion";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 //about data
 const about = {
@@ -51,7 +52,7 @@ const experience = {
     {
       company: "RX Health Info Systems",
       title: "Junior Developer",
-      date: "2023 - 2024",
+      date: "October 2023 - October 2024",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quae ",
     },
@@ -169,9 +170,51 @@ const Resume = () => {
 
           {/* content */}
           <div className="min-h-[70vh] w-full">
+            {/* education */}
+            <TabsContent value="education" className="w-full">
+              Education
+            </TabsContent>
+
             {/* experience */}
             <TabsContent value="experience" className="w-full">
-              Experience
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{experience.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {experience.description}
+                </p>
+
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {experience.items.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+                        >
+                          <span className="text-accent">{item.date}</span>
+                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
+                            {item.title}
+                          </h3>
+
+                          <div className="flex items-center gap-3">
+                            <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                            <p className="text-white/60">{item.company}</p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+
+            {/* skills */}
+            <TabsContent value="skills" className="w-full">
+              Skills
+            </TabsContent>
+            {/* About Me */}
+            <TabsContent value="about" className="w-full">
+              About Me
             </TabsContent>
           </div>
         </Tabs>
