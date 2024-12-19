ac230a1475e4d7a0bf2fa56efe5d040650ca3f74
Phuse21 <phusedallas@gmail.com>
2024-12-19 15:07:39 -0500

    mode-contact

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -48,9 +48,9 @@ const Contact = () => {
         <div className="flex flex-col xl:flex-row gap-[30px]">
           {/* form */}
           <div className="xl:w-[54%] order-2 xl:order-none">
-            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+            <form className="flex flex-col gap-6 p-10 bg-[#27272c]/5 dark:bg-[#27272c] rounded-xl">
               <h3 className="text-4xl text-accent">Let's work together</h3>
-              <p className="text-white/60">
+              <p className="text-[#1c1c22]/80 dark:text-[#ffffff]/60">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit Quos,
                 quae.
               </p>
@@ -99,7 +99,7 @@ const Contact = () => {
               {info.map((item, index) => {
                 return (
                   <li key={index} className="flex items-center gap-6">
-                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] flex justify-center items-center rounded-[5px] text-accent bg-[#27272c]">
+                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] flex justify-center items-center rounded-[5px] text-accent dark:bg-[#27272c] bg-[#ffffff]">
                       <div className="text-[28px] ">{item.icon}</div>
                     </div>
 
--- components/ui/select.jsx
+++ components/ui/select.jsx
@@ -17,7 +17,7 @@ const SelectTrigger = React.forwardRef(
     <SelectPrimitive.Trigger
       ref={ref}
       className={cn(
-        "flex h-[48px] w-full items-center justify-between rounded-[5px] border border-white/10 bg-primary px-4 py-5 text-base text-white/60 placeholder:text-white/10 focus:border-accent outline-none",
+        "flex h-[48px] w-full items-center justify-between rounded-[5px] border border-[#1c1c22]/50 dark:border-[#ffffff]/10 bg-primary px-4 py-5 text-base text-white/60 placeholder:text-white/10 focus:border-accent outline-none",
         className
       )}
       {...props}
@@ -70,7 +70,7 @@ const SelectContent = React.forwardRef(
       <SelectPrimitive.Content
         ref={ref}
         className={cn(
-          "relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border border-white/10 bg-primary text-white shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 dark:border-slate-800 dark:bg-slate-950 dark:text-slate-50",
+          "relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-[5px] border dark:border-[#ffffff]/10 border-[#1c1c22]/50 dark:bg-[#27272c] bg-[#ffffff] text-white shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 dark:border-slate-800 dark:text-slate-50",
           position === "popper" &&
             "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
           className
