909e5cef85dd605abd341930f169f39d663b56fa
Phuse21 <phusedallas@gmail.com>
2024-12-21 08:52:15 -0500

    slide buttons

--- components/WorkSliderBtns.jsx
+++ components/WorkSliderBtns.jsx
@@ -1,16 +1,33 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSwiper } from "swiper/react";
 import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi";
 
 const WorkSliderBtns = ({ containerStyles, iconStyles, btnStyles }) => {
   const swiper = useSwiper();
+  const [isBeginning, setIsBeginning] = useState(true);
+  const [isEnd, setIsEnd] = useState(false);
+
+  useEffect(() => {
+    const updateButtonStates = () => {
+      setIsBeginning(swiper.isBeginning);
+      setIsEnd(swiper.isEnd);
+    };
+
+    swiper.on("slideChange", updateButtonStates);
+    updateButtonStates();
+
+    return () => {
+      swiper.off("slideChange", updateButtonStates);
+    };
+  }, [swiper]);
+
   useEffect(() => {
     const handleKeyDown = (event) => {
-      if (event.key === "ArrowLeft") {
+      if (event.key === "ArrowLeft" && !isBeginning) {
         swiper.slidePrev();
-      } else if (event.key === "ArrowRight") {
+      } else if (event.key === "ArrowRight" && !isEnd) {
         swiper.slideNext();
       }
     };
@@ -19,14 +36,28 @@ const WorkSliderBtns = ({ containerStyles, iconStyles, btnStyles }) => {
     return () => {
       document.removeEventListener("keydown", handleKeyDown);
     };
-  }, [swiper]);
+  }, [swiper, isBeginning, isEnd]);
 
   return (
     <div className={containerStyles}>
-      <button className={btnStyles} onClick={() => swiper.slidePrev()}>
+      {/* Disable previous button on the first slide */}
+      <button
+        className={`${btnStyles} ${
+          isBeginning ? "opacity-50 cursor-not-allowed" : ""
+        }`}
+        onClick={() => swiper.slidePrev()}
+        disabled={isBeginning}
+      >
         <PiCaretLeftBold className={iconStyles} />
       </button>
-      <button className={btnStyles} onClick={() => swiper.slideNext()}>
+      {/* Disable next button on the last slide */}
+      <button
+        className={`${btnStyles} ${
+          isEnd ? "opacity-50 cursor-not-allowed" : ""
+        }`}
+        onClick={() => swiper.slideNext()}
+        disabled={isEnd}
+      >
         <PiCaretRightBold className={iconStyles} />
       </button>
     </div>
